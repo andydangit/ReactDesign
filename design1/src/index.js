26a75350cba34c95cf1a56308dbf3ed71aa922fc
pandoragear <andy.dang.it@gmail.com>
2021-03-11 20:35:00 -0600

    Add Semantic

--- design1/src/index.js
+++ design1/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import InputField from './InputField';
 
 const App = () => {
   return(
   <div className="container">
-      <text> this is a test </text>
+      <InputField/>
 
   </div>
   );
