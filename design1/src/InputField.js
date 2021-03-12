df5b8d10b5c2f353e1e96ac88ec7cd31c929950b
pandoragear <andy.dang.it@gmail.com>
2021-03-11 20:35:59 -0600

    create Input Field componet

---
+++ design1/src/InputField.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InputField = () => {
+    return (
+        <div className="ui input">
+        <input type="text" placeholder="Search..." class=""/>
+    </div>
+    )
+};
+
+export default InputField;
