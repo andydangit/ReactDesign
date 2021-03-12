49ffb3592d2df5e998279711c860c2fa6b2b062e
pandoragear <andy.dang.it@gmail.com>
2021-03-11 21:53:22 -0600

    Add Button component

---
+++ design1/src/button.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const ButtonColor = () => {
+    return (
+        <div> 
+            <button style = {{ backgroundColor: '#FF875B', color: 'white'}}> Create Account  </button>
+            </div> 
+    )
+}
+
+export default ButtonColor;
--- design1/src/index.js
+++ design1/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import InputField from './InputField';
+import ButtonColor from './button';
 
 const App = () => {
   return(
   <div className="container">
+    
       <InputField />
+      <ButtonColor/> 
 
   </div>
   );
