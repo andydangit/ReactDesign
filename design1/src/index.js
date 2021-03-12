761e793e3d5cec3ffac6cca16624a98381767b78
pandoragear <andy.dang.it@gmail.com>
2021-03-11 23:50:47 -0600

    Added Header component

--- design1/src/index.js
+++ design1/src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from "react-dom";
 import InputField from "./InputField";
 import ButtonColor from "./button";
 import BackgroundBorder from "./border";
+import HeaderStyle from './header';
+import './App.css';
+
+
 
 const App = () => {
   return (
     <div className="container">
       <BackgroundBorder>
+        <HeaderStyle />
         <InputField />
         <ButtonColor />
+        
       </BackgroundBorder>
     </div>
   );
 };
+
+
 
 ReactDOM.render(<App />, document.querySelector("#root"));
