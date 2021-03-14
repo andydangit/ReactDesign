1dbc588f7d830160d2a60e6cb1a273f71ea216ce
pandoragear <andy.dang.it@gmail.com>
2021-03-13 23:57:47 -0600

    Adding top text

--- design1/src/InputField.js
+++ design1/src/InputField.js
@@ -3,8 +3,10 @@ import Button from './button';
 
 const InputField = (props) => {
   return (
-    <div className="inputName">
-      <div className="ui input" style = {{ width: "350px"}}>
+    <div className = "container"> <text style = {{fontSize: "20px", fontWeight: "bold"}}>Sign up</text> 
+    <text style = {{fontSize: "15px", fontWeight: "bold", marginLeft: "290px"}}>X</text> 
+    <div className="inputName"> 
+      <div className="ui input" style = {{ width: "350px", marginTop: "25px"}}>
         <input type="text" placeholder="Name"  />
       </div>
       <div className="inputEmail">
@@ -21,6 +23,7 @@ const InputField = (props) => {
         </div> 
         <div style ={{ fontSize: "11px", marginLeft: " 118px", marginTop: "20px", color: "gray"}}> Already have an account</div>
       </div>
+    </div>
     </div>
   );
 };
