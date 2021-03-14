de3ebc7beaa24b0bd5c996c1dcaf5bbc53e7347e
pandoragear <andy.dang.it@gmail.com>
2021-03-14 00:20:15 -0600

    add style height and width

--- design1/src/button.js
+++ design1/src/button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = () => {
     return (
         <div> 
-            <button style = {{ backgroundColor: '#FF875B', color: 'white'}}> Create Account  </button>
+            <button style = {{ backgroundColor: '#FF875B', color: 'white', borderRadius: "3px", border: "0", height: "25px", width:"120px", fontSize:"12px"}}> Create Account  </button>
             </div> 
     )
 }
