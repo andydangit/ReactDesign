c851fe6401a3c9ead290c70cf03a947da1d8015a
pandoragear <andy.dang.it@gmail.com>
2021-03-13 23:50:02 -0600

    Added more style color to button and text

--- design1/src/InputField.js
+++ design1/src/InputField.js
@@ -4,21 +4,22 @@ import Button from './button';
 const InputField = (props) => {
   return (
     <div className="inputName">
-      <div className="ui input">
+      <div className="ui input" style = {{ width: "350px"}}>
         <input type="text" placeholder="Name"  />
       </div>
       <div className="inputEmail">
-        <div className="ui input">
+        <div className="ui input" style = {{ width: "350px"}}>
           <input type="email" placeholder="Email" />
         </div>
       <div className="inputPassword">
-        <div className="ui input">
+        <div className="ui input" style = {{ width: "350px"}}>
           <input type="password" placeholder="Password" />
         </div>
         </div> 
-        <div style = {{ marginLeft: "40px", marginTop: "10px"}}>
+        <div style = {{ marginLeft: "130px", marginTop: "25px"}}>
         <Button /> 
         </div> 
+        <div style ={{ fontSize: "11px", marginLeft: " 118px", marginTop: "20px", color: "gray"}}> Already have an account</div>
       </div>
     </div>
   );
